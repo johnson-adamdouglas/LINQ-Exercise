d1777b0ee43171e83bfe718d02e195022f92b86c
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-11-07 11:52:22 -0500

    completed exercise 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a list of video game names...
+            //Order the list of games by length of the game name.
+            //Use the lambda expression in this exercise as well.
+            //use Method Syntax for this exercise
+            var fishSpecies = new List<string> { "Largemouth Bass", "Smallmouth Bass", "Yellow Perch", "White Perch", "Black Crappie", 
+                "White Crappie", "Walleye", "Sauger", "Saugeye", "Bluegill" };
+
+            var speciesByLength = fishSpecies.OrderBy(x => x.Length).ToList();
+
+            foreach(var fish in speciesByLength)
+            {
+                Console.WriteLine(fish);
+            }
+            
         }
     }
 }
